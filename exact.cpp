093d5646e0a2f258f0a220c7e1d5c105a9ebad98
AndreiiArhire <andreiarhire2708@gmail.com>
2023-05-30 01:36:28 +0300

    exact source refactoring

--- exact.cpp
+++ exact.cpp
@@ -1,6 +1,7 @@
 #pragma GCC optimize("Ofast")
 #pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
 #pragma GCC optimization("unroll-loops")
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -37,31 +38,29 @@ void ckeck_force_stop() {
 
 int customRand() {
 	randSeed = (randSeed * 1000000103 + 27082000) % 1000000009;
-	return randSeed;
+	return (int)(randSeed);
 }
 
-set<int> all_nodes_set;
-vector<vector<int>> components;
-vector<unordered_set<int>>edges;
-vector<unordered_set<int>>red;
-vector<unordered_set<int>>red_bound;
-vector<unordered_set<int>>black;
-vector<unordered_set<int>>black_bound;
-vector<int> paired;
-vector<int> paired2;
-vector<int> poss;
 string s;
-vector<pair<int, int>>  best_solution, best_solution_all_graph;
-vector<int> nodes, ordered_nodes;
-unordered_set<int> neighbors, temp, nodes2;
+set<int> all_nodes_set;
+vector<vector<int>> components, temp_edges;
+vector<unordered_set<int>>edges, red, red_bound, black, black_bound;
+vector<int> paired, paired2, poss, nodes, ordered_nodes, bestDegree, temp_vector, t1, t2, t3, temp_visited, deg_vec, red_edge;
+vector<pair<int, int>>  best_solution, best_solution_all_graph, temp1, hashes, t4, edge_pairs;
+unordered_set<int> neighbors, temp, nodes2, nbr, all_nodes, current_nodes, new_red_edges;
 vector < pair < int, pair < int, int > > > choices;
-vector<int> bestDegree;
-int best_red_degree = (int)1e9, best_red_degree_all_graph;
-vector<int>temp_vector;
-vector<  pair <int, int > > temp1;
-vector< pair <int, int> > hashes;
-vector<vector<char>>adjacency;
-int STEP, LOWER_BOUND = -1;
+int best_red_degree = (int)1e9, best_red_degree_all_graph, STEP, LOWER_BOUND = -1, lower_bound_tl;
+int global_degree, global_upperbound, global_lowerbound;
+
+struct conex_comp {
+	vector<int> nodes;
+	vector<pair<int, int>> upper_bound_operations;
+	int best_red_degree = 0;
+	int lowerbound = 0;
+};
+
+vector<conex_comp> conn;
+
 struct partial_solution_hash {
 	vector < pair <int, int> > node_pairs;
 	int max_red_degree = 0;
@@ -70,27 +69,17 @@ struct partial_solution_hash {
 
 vector<partial_solution_hash> current_hash_solutions[2];
 
-unordered_set<int> all_nodes, current_nodes;
-
-vector<int> t1, t2, t3;
-unordered_set<int> nbr;
-vector<pair <int, int >> t4;
-vector< vector<int> > temp_edges;
-vector<int> temp_visited;
-int lower_bound_tl;
 struct pair_hash {
 	template <class T1, class T2>
 	std::size_t operator () (const std::pair<T1, T2>& p) const {
 		auto a = std::hash<T1>{}(p.first);
 		auto b = std::hash<T2>{}(p.second);
-		// Cantor function for pairing hash
+		/* Cantor function for pairing hash */
 		return  1ll * (1ll * (a + b) * (a + b + 1) / 2 + 1ll * b) % 1000000009;
 	}
 };
 
-
 unordered_map < pair <int, int>, int, pair_hash > hash_to_index;
-//map < pair <int, int>, int > hash_to_index;
 
 double getElapsed() {
 	auto end = std::chrono::high_resolution_clock::now();
@@ -113,35 +102,14 @@ void init_data() {
 	poss.resize(n + 5, 0);
 	paired2.resize(n + 5, 0);
 	temp_visited.resize(n + 5, 0);
-	adjacency.resize(n + 5, vector<char>(n + 5, 0));
 	for (int i = 1; i <= 3 * n; ++i) {
 		hashes[i].first = 1ll * hashes[i - 1].first * PRIME1 % MOD1;
 		hashes[i].second = 1ll * hashes[i - 1].second * PRIME2 % MOD2;
 	}
 }
 
-void clear_data() {
-	edges.clear();
-	red.clear();
-	black.clear();
-	paired.clear();
-	paired2.clear();
-	nodes.clear();
-	ordered_nodes.clear();
-	neighbors.clear();
-	choices.clear();
-}
 
 void readData() {
-	string path_input = "tests//exact//exact_";
-	for (int l = 1; l <= 3 - to_string(testNo).size(); ++l) {
-		path_input.push_back('0');
-	}
-	path_input += to_string(testNo);
-	path_input += ".gr";
-	//ifstream in(path_input);
-	cin.tie();
-	cout.tie();
 	string problem_id;
 	cin >> problem_id >> problem_id >> n >> m;
 	init_data();
@@ -151,7 +119,6 @@ void readData() {
 		edges[x].insert(y);
 		edges[y].insert(x);
 	}
-	//in.close();
 	if (n == 0) {
 		exit(0);
 	}
@@ -182,7 +149,7 @@ void split_into_components() {
 			components.emplace_back(curr_component);
 		}
 	}
-	sort( components.begin(), components.end(), [] (vector<int> i, vector<int> j){ return i.size() > j.size(); } );
+	sort(components.begin(), components.end(), [](vector<int> i, vector<int> j) { return i.size() > j.size(); });
 }
 
 pair <int, vector<pair<int, int>>> get_initial_upper_bound(int cc) {
@@ -218,7 +185,7 @@ pair <int, vector<pair<int, int>>> get_initial_upper_bound(int cc) {
 					swap(x, y);
 				}
 				int common_both_black = 0, common_mix = 0, common_both_red = 0, edges_between = 0, max_red_degree = 0, new_red_change = 0, new_red_not_change = 0;
-				int cnt_max = black[x].size() + red[x].size();
+				int cnt_max = (int)(black[x].size() + red[x].size());
 				if (black[x].count(y) || red[x].count(y)) {
 					edges_between = 2;
 				}
@@ -354,7 +321,7 @@ pair < int, vector< pair <int, int > > > solve_bound(int cc, int hash_id, int st
 					swap(x, y);
 				}
 				int common_both_black = 0, common_mix = 0, common_both_red = 0, edges_between = 0, max_red_degree = 0, new_red_change = 0, new_red_not_change = 0;
-				int cnt_max = black_bound[x].size() + red_bound[x].size();
+				int cnt_max = (int)(black_bound[x].size() + red_bound[x].size());
 				if (black_bound[x].count(y) || red_bound[x].count(y)) {
 					edges_between = 2;
 				}
@@ -566,15 +533,15 @@ int build_graph_from_hash(const int& cc, const int& hash_id, const int& step) {
 	ckeck_force_stop();
 
 	if (current_hash_solutions[step][hash_id].node_pairs.size() == components[cc].size()
-	 && current_hash_solutions[step][hash_id].max_red_degree <  best_red_degree)	{
-		best_red_degree = current_hash_solutions[step][hash_id].max_red_degree ;
+		&& current_hash_solutions[step][hash_id].max_red_degree < best_red_degree) {
+		best_red_degree = current_hash_solutions[step][hash_id].max_red_degree;
 		best_solution = current_hash_solutions[step][hash_id].node_pairs;
 		return 0;
 	}
 
 	nodes_left = unordered_set <int>();
 	pair < int, vector<pair <int, int>> > ret = make_pair(1e9, vector<pair<int, int>>());
-	if ((LOWER_BOUND == -1 && current_hash_solutions[step][hash_id].max_red_degree >= best_red_degree - 1 /* && STEP >= 2*/) || (LOWER_BOUND != -1 && current_hash_solutions[step][hash_id].max_red_degree >= LOWER_BOUND)) {
+	if ((LOWER_BOUND == -1 && current_hash_solutions[step][hash_id].max_red_degree >= best_red_degree - 1) || (LOWER_BOUND != -1 && current_hash_solutions[step][hash_id].max_red_degree >= LOWER_BOUND)) {
 		ret = solve_bound(cc, hash_id, step);
 		int upper_bound = ret.first;
 		int max_red_degree = max(ret.first, current_hash_solutions[step][hash_id].max_red_degree);
@@ -594,15 +561,15 @@ int build_graph_from_hash(const int& cc, const int& hash_id, const int& step) {
 	}
 	return to_be_continued;
 }
+
 int dist_cel_mult_2, muchie_rosie_creata;
+
 pair <int, int> get_red_degree_by_simulation(int x, int y) {
 	int  max_red_degree = 0, val = 0, muchii_rosii_noi = 0;
 	dist_cel_mult_2 = 0;
 	muchie_rosie_creata = 0;
-	// x fixat -> 1
-	// y fixat -> 2
 	int common_both_black = 0, common_mix = 0, common_both_red = 0, edges_between = 0;
-	int cnt_max = black[x].size() + red[x].size();
+	int cnt_max = (int)(black[x].size() + red[x].size());
 	if (red[x].count(y)) {
 		muchii_rosii_noi--;
 	}
@@ -749,12 +716,6 @@ vector< pair <int, int> > reduce_component(int cc) {
 	return solution;
 }
 
-
-vector<pair <int, int>> edge_pairs;
-vector < int> deg_vec;
-vector < int> red_edge;
-unordered_set<int> new_red_edges;
-
 void branch_and_bound(int cc) {
 	partial_solution_hash emprty_solution;
 	current_hash_solutions[0].emplace_back(emprty_solution);
@@ -779,7 +740,7 @@ void branch_and_bound(int cc) {
 			}
 			int number_of_red_edges = 0;
 			for (auto i : components[cc]) {
-				number_of_red_edges += red_bound[i].size();
+				number_of_red_edges += (int)(red_bound[i].size());
 			}
 			number_of_red_edges /= 2;
 			for (auto node_pair : current_hash_solutions[step][hash_id].node_pairs) {
@@ -798,7 +759,7 @@ void branch_and_bound(int cc) {
 				current_nodes.erase(node.second);
 			}
 			for (auto it : current_nodes) {
-				t1.emplace_back(it);				// nu sunt
+				t1.emplace_back(it);
 			}
 			sort(t1.begin(), t1.end());
 
@@ -817,7 +778,7 @@ void branch_and_bound(int cc) {
 						edge_pairs.emplace_back(t1[i], t1[j]);
 						deg_vec.emplace_back(deg);
 						red_edge.emplace_back(number_of_red_edges + new_red_edges_count);
-						i = t1.size();
+						i = (int)(t1.size());
 						break;
 					}
 					edge_pairs.emplace_back(t1[i], t1[j]);
@@ -836,7 +797,7 @@ void branch_and_bound(int cc) {
 					new_hash.number_of_red_edges = red_edge[l];
 					new_hash.node_pairs = solution_hash.node_pairs;
 					new_hash.node_pairs.emplace_back(pairr);
-					hash_to_index[curr_hash] = current_hash_solutions[1 ^ step].size();
+					hash_to_index[curr_hash] = (int)(current_hash_solutions[1 ^ step].size());
 					current_hash_solutions[1 ^ step].emplace_back(new_hash);
 				}
 				else
@@ -883,7 +844,6 @@ void branch_and_bound(int cc) {
 
 
 int get_lowerbound(int cc, int stop) {
-	/* upperbound is required */
 	ckeck_force_stop();
 	int lower_bound = 0;
 	vector<int> nodes_to_be_erased;
@@ -997,7 +957,7 @@ int get_lowerbound(int cc, int stop) {
 				edges[nodes_to_be_erased[j]].clear();
 			}
 
-			best_red_degree = 1e9;
+			best_red_degree = (int)(1e9);
 			best_solution = vector<pair <int, int>>();
 			if (getElapsed() > lower_bound_tl) {
 				break;
@@ -1016,18 +976,17 @@ int get_lowerbound(int cc, int stop) {
 			}
 			lower_bound = max(lower_bound, best_red_degree);
 
-			best_red_degree = 1e9;
+			best_red_degree = (int)(1e9);
 			best_solution = vector<pair <int, int>>();
 			components[cc] = nodes_to_be_erased;
 			edges = edges2;
-			best_red_degree = 1e9;
+			best_red_degree = (int)(1e9);
 			best_solution.clear();
 		}
 	}
 	ckeck_force_stop();
 	return lower_bound;
 }
-
 
 
 int eval_state(int cc, vector<pair <int, int>>sol, int lim) {
@@ -1041,10 +1000,8 @@ int eval_state(int cc, vector<pair <int, int>>sol, int lim) {
 			return i;
 		}
 	}
-	return sol.size();
+	return (int)(sol.size());
 }
-
-
 
 
 vector< pair <int, int> > go_heuristic(int cc, vector< pair <int, int > > sol, int max_pos) {
@@ -1086,7 +1043,7 @@ vector< pair <int, int> > go_heuristic(int cc, vector< pair <int, int > > sol, i
 					swap(x, y);
 				}
 				int common_both_black = 0, common_mix = 0, common_both_red = 0, edges_between = 0, max_red_degree = 0, new_red_change = 0, new_red_not_change = 0;
-				int cnt_max = black[x].size() + red[x].size();
+				int cnt_max = (int)(black[x].size() + red[x].size());
 				if (black[x].count(y) || red[x].count(y)) {
 					edges_between = 2;
 				}
@@ -1186,58 +1143,57 @@ pair <int, vector<pair <int, int>>> find_heuristic_upper_bound(int cc, vector<pa
 	int equal_ = 0;
 	for (l = 1; l <= 500000 && max_pos != sol.size() && getElapsed() < lower_bound_tl; ++l) {
 		vector<pair <int, int>> sol2 = sol;
-			for (int p = 1; p <= 1; ++p) {
-				for (auto i : components[cc]) {
-					nodes_left.insert(i);
-				}
-				int pos_to_change = customRand() % (max_pos + 1);
-				for (int i = 0; i <= pos_to_change; ++i) {
-					nodes_left.erase(sol2[i].second);
-				}
-				vector<int> lef;
-				for (auto i : nodes_left) {
-					lef.emplace_back(i);
-				}
-				int random_el = lef[customRand() % lef.size()];
-				nodes_left = unordered_set<int>();
-				sol2[pos_to_change].first = random_el;
-
-				/////////////
-				pos_to_change = customRand() % (max_pos + 1);
-				nodes_left.insert(sol2[pos_to_change].second);
-				for (int i = pos_to_change + 1; i < sol2.size(); ++i) {
-					nodes_left.insert(sol2[i].first);
-					nodes_left.insert(sol2[i].second);
-				}
-				lef = vector<int>();
-				for (auto i : nodes_left) {
-					lef.emplace_back(i);
-				}
-				int rand_poz = customRand() % lef.size();
-				random_el = lef[rand_poz];
-				int val = sol2[pos_to_change].second;
-
-				if (sol2[pos_to_change].first == random_el) {
-					continue;
-				}
-				sol2[pos_to_change].second = random_el;
-				for (int i = pos_to_change + 1; i < sol2.size(); ++i) {
-					if (sol2[i].first == random_el) {
-						sol2[i].first = val;
-					}
-					if (sol2[i].second == random_el) {
-						sol2[i].second = val;
-					}
-				}
-				nodes_left = unordered_set<int>();
+		for (int p = 1; p <= 1; ++p) {
+			for (auto i : components[cc]) {
+				nodes_left.insert(i);
 			}
-		
+			int pos_to_change = customRand() % (max_pos + 1);
+			for (int i = 0; i <= pos_to_change; ++i) {
+				nodes_left.erase(sol2[i].second);
+			}
+			vector<int> lef;
+			for (auto i : nodes_left) {
+				lef.emplace_back(i);
+			}
+			int random_el = lef[customRand() % lef.size()];
+			nodes_left = unordered_set<int>();
+			sol2[pos_to_change].first = random_el;
+
+			pos_to_change = customRand() % (max_pos + 1);
+			nodes_left.insert(sol2[pos_to_change].second);
+			for (int i = pos_to_change + 1; i < sol2.size(); ++i) {
+				nodes_left.insert(sol2[i].first);
+				nodes_left.insert(sol2[i].second);
+			}
+			lef = vector<int>();
+			for (auto i : nodes_left) {
+				lef.emplace_back(i);
+			}
+			int rand_poz = customRand() % lef.size();
+			random_el = lef[rand_poz];
+			int val = sol2[pos_to_change].second;
+
+			if (sol2[pos_to_change].first == random_el) {
+				continue;
+			}
+			sol2[pos_to_change].second = random_el;
+			for (int i = pos_to_change + 1; i < sol2.size(); ++i) {
+				if (sol2[i].first == random_el) {
+					sol2[i].first = val;
+				}
+				if (sol2[i].second == random_el) {
+					sol2[i].second = val;
+				}
+			}
+			nodes_left = unordered_set<int>();
+		}
+
 		auto sol3 = go_heuristic(cc, sol2, max_pos);
 		int new_pos = eval_state(cc, sol2, lim);
 		int new_pos2 = eval_state(cc, sol3, lim);
-		if ( new_pos == max_pos )	{
+		if (new_pos == max_pos) {
 			equal_++;
-			if ( equal_ == 60 )	{
+			if (equal_ == 60) {
 				equal_ = 0;
 				sol = sol2;
 			}
@@ -1266,18 +1222,6 @@ pair <int, vector<pair <int, int>>> find_heuristic_upper_bound(int cc, vector<pa
 }
 
 
-struct conex_comp {
-	vector<int> nodes;
-	vector<pair<int ,int>> upper_bound_operations;
-	int best_red_degree = 0;
-	int lowerbound = 0;
-};
-
-vector<conex_comp> conn;
-
-int global_degree;
-int global_upperbound;
-int global_lowerbound;
 void solver() {
 
 	testNo = 138;
@@ -1285,27 +1229,27 @@ void solver() {
 	split_into_components();
 	int first_comp_representant = 0;
 	/* Initial Upperbound */
-	for ( int cc = 0; cc < components.size(); ++ cc )	{
+	for (int cc = 0; cc < components.size(); ++cc) {
 		conex_comp act;
 		auto sol = reduce_component(cc);
 		for (auto it : sol) {
 			best_solution_all_graph.emplace_back(it);
 		}
 		act.nodes = components[cc];
-		best_red_degree = 1e9;
-		best_solution = vector<pair<int ,int>>();
+		best_red_degree = (int)(1e9);
+		best_solution = vector<pair<int, int>>();
 		auto upperbound = get_initial_upper_bound(cc);
 		act.upper_bound_operations = upperbound.second;
 		act.best_red_degree = upperbound.first;
-		global_degree = max( global_degree, upperbound.first);
+		global_degree = max(global_degree, upperbound.first);
 		conn.emplace_back(act);
 	}
 	/* Hill Climbing Upperbound */
 	lower_bound_tl = 5 * 60;
-	for ( int best_degree = global_degree - 1; best_degree >= 0 && getElapsed() < lower_bound_tl; -- best_degree )	{
-		for ( int cc = 0; cc < components.size() && getElapsed() < lower_bound_tl; ++ cc){
+	for (int best_degree = global_degree - 1; best_degree >= 0 && getElapsed() < lower_bound_tl; --best_degree) {
+		for (int cc = 0; cc < components.size() && getElapsed() < lower_bound_tl; ++cc) {
 			auto new_sol = find_heuristic_upper_bound(cc, conn[cc].upper_bound_operations, best_degree);
-			if ( new_sol.first != -1 )	{
+			if (new_sol.first != -1) {
 				conn[cc].upper_bound_operations = new_sol.second;
 				conn[cc].best_red_degree = min(conn[cc].best_red_degree, best_degree);
 			}
@@ -1314,38 +1258,38 @@ void solver() {
 
 	/* Dynamic programming Lowerbound */
 	lower_bound_tl = 10 * 60;
-	for ( int cc = 0; cc < components.size() && getElapsed() < lower_bound_tl; ++ cc )	{
+	for (int cc = 0; cc < components.size() && getElapsed() < lower_bound_tl; ++cc) {
 		LOWER_BOUND = -1;
 		best_red_degree = conn[cc].best_red_degree;
 		best_solution = conn[cc].upper_bound_operations;
-		if ( conn[cc].best_red_degree >  global_lowerbound )	{
+		if (conn[cc].best_red_degree > global_lowerbound) {
 			LOWER_BOUND = get_lowerbound(cc, conn[cc].best_red_degree);
 			conn[cc].lowerbound = LOWER_BOUND;
 			global_lowerbound = max(global_lowerbound, LOWER_BOUND);
-		}	
+		}
 	}
 
 
 	/* Dynamic programming solver */
 	lower_bound_tl = 1000 * 60;
-	for ( int cc = 0; cc < components.size(); ++ cc )	{
-		if (conn[cc].best_red_degree <= global_lowerbound)	{
+	for (int cc = 0; cc < components.size(); ++cc) {
+		if (conn[cc].best_red_degree <= global_lowerbound) {
 			continue;
 		}
 		best_solution = conn[cc].upper_bound_operations;
-	 	best_red_degree = conn[cc].best_red_degree;
+		best_red_degree = conn[cc].best_red_degree;
 		branch_and_bound(cc);
 		conn[cc].upper_bound_operations = best_solution;
 	}
 
 
 	/* Link components */
-	for ( int cc = 0; cc < components.size(); ++ cc )	{
-		for ( auto it : conn[cc].upper_bound_operations )	{
+	for (int cc = 0; cc < components.size(); ++cc) {
+		for (auto it : conn[cc].upper_bound_operations) {
 			best_solution_all_graph.emplace_back(it);
 		}
 		if (cc > 0) {
-			if (conn[cc].upper_bound_operations .empty()) {
+			if (conn[cc].upper_bound_operations.empty()) {
 				best_solution_all_graph.emplace_back(first_comp_representant, components[cc].back());
 
 			}
@@ -1356,12 +1300,12 @@ void solver() {
 		}
 		else
 		{
-			if (conn[cc].upper_bound_operations .empty()) {
+			if (conn[cc].upper_bound_operations.empty()) {
 				first_comp_representant = components[cc].back();
 			}
 			else
 			{
-				first_comp_representant = conn[cc].upper_bound_operations .back().first;
+				first_comp_representant = conn[cc].upper_bound_operations.back().first;
 			}
 		}
 	}
